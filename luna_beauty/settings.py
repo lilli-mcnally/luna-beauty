79b402c5579d41d1200083e1c053367cca33f0ac
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-02-11 22:18:47 +0000

    Add Category and Product models

--- luna_beauty/settings.py
+++ luna_beauty/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,13 +106,17 @@ WSGI_APPLICATION = 'luna_beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    volume = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    image = models.ImageField(null=True, blank=True)
+    shades = ArrayField(models.CharField(max_length=200, null=True, blank=True))
+
+    def __str__(self):
+        return self.name

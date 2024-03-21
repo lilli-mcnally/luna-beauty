6628172c004098f51914e6068ffadbde7f0f4053
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-03-21 18:38:23 +0000

    Update checkout models logic

--- checkout/models.py
+++ checkout/models.py
@@ -28,12 +28,18 @@ class Order(models.Model):
 
     def update_total(self):
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.delivery_cost = settings.DELIVERY
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
 
     def save(self, *args, **kwargs):
-
         if not self.order_number:
             self.order_number = self_generate_order_number()
         super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -41,3 +47,10 @@ class OrderLineItem(models.Model):
     shade = models.CharField(max_length=20, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'

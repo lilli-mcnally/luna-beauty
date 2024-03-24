76d35556733c2d103746111f74075444fb022b3f
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-03-24 16:44:29 +0000

    Add Strip input to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OabozL6ZsSlUMJa5744nSeTKReXCDFvQXbF3Re5ovLrTFlWwpLrsmOO5s85DDRKZ19JhwOlaaq1WaMmVf7SfYzU00saXpEP2g',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

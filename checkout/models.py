e3f064005d39b9306e495d8b55e4b7e0430533a9
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-04-02 20:51:03 +0000

    Add discount to checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,5 @@
 import uuid
-import decimal
-
+from decimal import Decimal
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
@@ -21,6 +20,7 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=5, decimal_places=2, null=False, default=0)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
@@ -28,10 +28,16 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.delivery_cost = settings.DELIVERY
-        self.grand_total = self.order_total + decimal.Decimal(self.delivery_cost)
-        self.save()
+
+        if self.order_total >= settings.DISCOUNT_THRESHOLD:
+            self.discount = Decimal((self.order_total / 100) * settings.DISCOUNT_PERCENTAGE)
+            self.grand_total = (self.order_total + Decimal(self.delivery_cost) - self.discount )
+        else:
+            self.discount = 0
+            self.grand_total = self.order_total + Decimal(self.delivery_cost)
+            self.save()
 
 
     def save(self, *args, **kwargs):

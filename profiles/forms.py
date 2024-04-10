f15d58edfc85a3e7812593ba2ebc6a529c9c0b84
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-04-10 20:26:37 +0000

    Update and customise Allauth templates

---
+++ profiles/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+        'default_phone': 'Phone Number',
+        'default_street_address1': 'Street Address 1',
+        'default_street_address2': 'Street Address 2',
+        'default_town_or_city': 'Town or City',
+        'default_county': 'County',
+        'default_postcode': 'Postcode',
+        }
+
+        self.fields['default_phone'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+from .forms import UserProfileForm
+
+
 def profile(request):
     template = 'profiles/profile.html'
     context = {}
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders
+    }
 
     return render(request, template, context)

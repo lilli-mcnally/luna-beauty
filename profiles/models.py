9214c90c29366024d87caaaa085533f6b12f1cd4
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-04-11 19:51:50 +0000

    Update Profile page functionality

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     default_phone = models.CharField(max_length=100, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=100, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=100, null=True, blank=True)
@@ -19,8 +19,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-@receiver(post_save, sender=UserProfile)
+@receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-    # if created:
-    UserProfile.objects.create(user=instance)
-    # instance.userprofile.save()
+    if created and not hasattr(instance, 'userprofile'):
+        UserProfile.objects.create(user=instance)

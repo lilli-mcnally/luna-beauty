7e8aeb482259e76eca4c2a2cb4729694aff8f0e5
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-04-14 17:03:44 +0000

    Update profile template and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile successfully updated')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)

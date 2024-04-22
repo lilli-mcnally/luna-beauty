8bccbe020bee209cfdf5c5a7c722707ffe6c173a
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2024-04-22 09:38:06 +0000

    Add responsive design for nav and footer

--- luna_beauty/settings.py
+++ luna_beauty/settings.py
@@ -192,11 +192,9 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
-    print('development')
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'orders@lunabeauty.co.uk'
 else:
-    print('production')
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587

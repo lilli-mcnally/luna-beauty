318ea6f9ea54e6705e1dbb807120803f50e2712b
lilli-mcnally <Lilli.lawlor@hotmail.co.uk>
2025-10-21 21:17:38 +0000

    Update allauth middleware

--- luna_beauty/settings.py
+++ luna_beauty/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

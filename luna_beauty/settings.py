6ca98589b5ba62ae85a78674aedfdd3ca7cca73f
lilli-mcnally <Lilli.lawlor@hotmail.co.uk>
2025-10-21 21:40:46 +0000

    Add string to static and media

--- luna_beauty/settings.py
+++ luna_beauty/settings.py
@@ -180,10 +180,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = str('/static/')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = str('/media/')
 MEDIA_ROOT = BASE_DIR / "media"
 
 if 'USE_AWS' in os.environ:

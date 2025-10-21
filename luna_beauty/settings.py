008113d20895db10933691efe0435a73a9df4623
lilli-mcnally <Lilli.lawlor@hotmail.co.uk>
2025-10-21 20:33:53 +0000

    Update Database_URL

--- luna_beauty/settings.py
+++ luna_beauty/settings.py
@@ -126,9 +126,14 @@ WSGI_APPLICATION = 'luna_beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+        )
     }
 else:
     DATABASES = {
